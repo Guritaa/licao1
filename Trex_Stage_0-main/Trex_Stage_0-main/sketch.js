56a7ada8729f55dde94863161c8200c3e1d3353b
Guritaa <131707995+Guritaa@users.noreply.github.com>
2023-04-26 16:11:02 -0300

    Add files via upload

---
+++ Trex_Stage_0-main/Trex_Stage_0-main/sketch.js
@@ -0,0 +1,30 @@
+
+var trex ,trex_running,chao;
+
+function preload(){
+  trex_running=loadAnimation("trex1.png","trex3.png","trex4.png");
+  
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+ chao=createSprite(200,180,400,20);
+ trex=createSprite(50,170,20,50);
+ trex.addAnimation("running",trex_running);
+ trex.scale=0.5;
+}
+
+function draw(){
+  background("green")
+
+  if(keyDown("space")){
+    trex.velocityY=-10;
+  }
+
+  trex.velocityY += 0.8;
+
+  trex.collide(chao)
+  
+  drawSprites();
+}
